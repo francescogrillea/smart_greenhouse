20fc2545f7bdee18d24ebced1ef4d4baf425fab2
git-marco00 <98462244+git-marco00@users.noreply.github.com>
2023-05-18 11:56:19 -0400

    bug fixed

--- coap-node/project-conf.h
+++ coap-node/project-conf.h
@@ -1,44 +1,32 @@
-/*
- * Copyright (c) 2013, Institute for Pervasive Computing, ETH Zurich
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- */
+#ifndef __PROJECT_CONF_H
+#define __PROJECT_CONF_H
 
-/**
- * \file
- *      Erbium (Er) example project configuration.
- * \author
- *      Matthias Kovatsch <kovatsch@inf.ethz.ch>
- */
+///////////////////////// COAP //////////////////////
 
-#ifndef PROJECT_CONF_H_
-#define PROJECT_CONF_H_
+// Set the max response payload before enable fragmentation:
+#undef REST_MAX_CHUNK_SIZE
+#define REST_MAX_CHUNK_SIZE 64
+// Set the maximum number of CoAP concurrent transactions:
+#undef COAP_MAX_OPEN_TRANSACTIONS
+#define COAP_MAX_OPEN_TRANSACTIONS 4
+// save some memory for the sky platform
+#undef NBR_TABLE_CONF_MAX_NEIGHBORS
+#define NBR_TABLE_CONF_MAX_NEIGHBORS 10
+#undef UIP_CONF_MAX_ROUTES
+#define UIP_CONF_MAX_ROUTES 10
+#undef UIP_CONF_BUFFER_SIZE
+#define UIP_CONF_BUFFER_SIZE 240
 
-#define LOG_LEVEL_APP LOG_LEVEL_DBG
+////////////////////////////////////////////////
 
-#endif /* PROJECT_CONF_H_ */
+
+#undef QUEUEBUF_CONF_NUM
+#define QUEUEBUF_CONF_NUM	32
+
+#undef IEEE802154_CONF_PANID
+#define IEEE801254_CONF_PANID 0x0011
+
+#undef IEEE802154_CONF_DEFAULT_CHANNEL
+#define IEEE802154_CONF_DEFAULT_CHANNEL 15
+
+#endif
