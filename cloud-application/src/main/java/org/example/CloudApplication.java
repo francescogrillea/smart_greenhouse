f546ac5c97858e25b91fcd06a5036ef4ca3e954f
git-marco00 <98462244+git-marco00@users.noreply.github.com>
2023-05-24 06:23:26 -0400

    Add files via upload
    final cloud-application

---
+++ cloud-application/src/main/java/org/example/CloudApplication.java
@@ -0,0 +1,26 @@
+package org.example;
+
+
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CloudApplication{
+    public static void main( String[] args ){
+
+        // MySQL Connection
+        DatabaseHandler databaseHandler = new DatabaseHandler();
+
+
+        // CoAP Server that stores actuators info in a MySQL DB
+        CoapServer server = new CoapServer();
+        server.add(new CoapRegistrationResource("registration", databaseHandler));
+        server.start();
+
+        // MQTT subscriber launch
+        MqttSubscriber myMQTTSubscriber = new MqttSubscriber(databaseHandler);
+    }
+}
