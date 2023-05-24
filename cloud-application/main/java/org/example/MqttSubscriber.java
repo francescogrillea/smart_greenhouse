2eeceebefdfdb2ac4ada891afe7dc7e7365fa253
git-marco00 <98462244+git-marco00@users.noreply.github.com>
2023-05-24 06:22:13 -0400

    Add files via upload
    added cloud-application final

---
+++ cloud-application/main/java/org/example/MqttSubscriber.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.paho.client.mqttv3.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Arrays;
+
+public class MqttSubscriber implements MqttCallback {
+    private final String topic ="temperature";
+    private final String broker = "tcp://127.0.0.1:1883";
+    private final String clientId = "SmartGreenhouse";
+    private DatabaseHandler databaseHandler;
+    public MqttSubscriber(DatabaseHandler databaseHandler){
+        this.databaseHandler = databaseHandler;
+        try {
+            MqttClient mqttClient = new MqttClient(broker, clientId);
+            mqttClient.setCallback(this);
+            mqttClient.connect();
+            mqttClient.subscribe(topic);
+        } catch (MqttException me){
+            me.printStackTrace();
+        }
+    }
+
+    @Override
+    public void connectionLost(Throwable throwable) {
+        System.out.println("Connection with broker lost..." + throwable.getMessage());
+    }
+
+    @Override
+    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+        try {
+            // message unpacking
+            JSONObject requestJson = (JSONObject) JSONValue.parseWithException(new String(mqttMessage.getPayload()));
+            int temp = Integer.parseInt(requestJson.get("temperature").toString());
+            String ip = requestJson.get("ip").toString();
+
+            double temp_double = (double) temp / 10;
+            // adding to the database
+            databaseHandler.addTemperature(temp_double, ip);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+        System.out.println("Delivery completed!");
+    }
+}
